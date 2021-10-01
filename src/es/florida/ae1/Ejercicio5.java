92d4e1f724d603fc64bcc3814a19f56200f32cf7
Balbastrex <57754728+balbastrex@users.noreply.github.com>
2021-10-01 18:05:16 +0200

    AE1-5

--- src/es/florida/ae1/Ejercicio5.java
+++ src/es/florida/ae1/Ejercicio5.java
@@ -3,6 +3,7 @@ package es.florida.ae1;
 public class Ejercicio5 {
 	
 	static int listFilter(int[] llista){
+		
 		int max = 0;
 		for (int i = 0; i<llista.length; i++) {
 			if (llista[i] > max) {
@@ -12,9 +13,8 @@ public class Ejercicio5 {
 		return max;
 	}
 
-	public static void main(int[] args) {
-		
-		int[] llista = {1,2,3,4,5,6,7,8};
+	public static void main(String[] args) {
+		int[] llista = {1,2,3,10,5,6,7,8};
 		System.out.println(listFilter(llista));
 		
 		
